06b279e7744718fad4c350f9116e38248eea3e2d
Sergio V Ferreiras <sferreirasg@gmail.com>
2022-09-20 15:12:19 -0400

    This comment is after the branch was created.

--- src/tree/MyTree.java
+++ src/tree/MyTree.java
@@ -10,6 +10,7 @@ public class MyTree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello Git, from branch bug#1234 !!");	
 		int i, j;
 
 		for(i=1;i<=10;i++)
@@ -27,6 +28,7 @@ public class MyTree {
 		
 		
 		System.out.println("SFerreiras- First modification to Git.");
+		
 
 		
 	}
